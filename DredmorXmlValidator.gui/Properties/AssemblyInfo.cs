3c70ca861dc92b1ebd19b82ce9336baf0935038f
Glazed <qxhqx3@gmail.com>
2012-06-20 00:24:25 -0400

    Added shared AssemblyVersion.cs to every project

--- DredmorXmlValidator.gui/Properties/AssemblyInfo.cs
+++ DredmorXmlValidator.gui/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid( "4fb0622d-7cf7-4cab-9ed2-28b75e5c30d8" )]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "0.4.2.0" )]
-[assembly: AssemblyFileVersion( "0.4.2.0" )]
