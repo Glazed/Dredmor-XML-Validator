c1c563a71817878a53689f8a90fb70b8900b1226
Glazed <qxhqx3@gmail.com>
2012-07-30 16:38:39 -0400

    Made monster dash spells optional.

--- References/AssemblyVersion.cs
+++ References/AssemblyVersion.cs
@@ -1,5 +1,5 @@
 ﻿//Copyright (c) 2012 Gaslamp Games Inc. See license.txt for license information.
 using System.Reflection;
 
-[assembly: AssemblyVersion( "1.1.0.0" )]
-[assembly: AssemblyFileVersion( "1.1.0.0" )]
+[assembly: AssemblyVersion( "1.1.1.0" )]
+[assembly: AssemblyFileVersion( "1.1.1.0" )]
